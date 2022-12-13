9a7f3d54a888de3c7fa3612d4518bce43a321bf2
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-13 14:27:53 -0800

    commit tree 9ef910bbb566c53848194d39e50758407ec48f40

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = []; 
+
+function writeCards(array, event) {
+    const newNames = [...names]
+
+    for (let i = 0; i < array.length; i++) {
+        newNames.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return newNames;
+}
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num)
+        num--;
+    }
+};
